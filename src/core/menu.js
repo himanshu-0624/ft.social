26dc5509b5d69802df2672a1cdd0217917b2ef77
Himanshu Gupta <himbxr22@gmail.com>
2020-10-06 15:00:20 +0530

    commit

--- src/core/menu.js
+++ src/core/menu.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment } from 'react';
 import { Link, withRouter  } from "react-router-dom";
 
 
--- src/profile/info.js
+++ src/profile/info.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import abc from "./abc.jpg"
 
 const Info = () => {
